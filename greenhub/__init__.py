3b3193bfa94d76c98b9220338faec28dae499a86
Alvaro Mateo <alvaromateo9@gmail.com>
2018-05-26 11:39:55 +0200

    Finished packaging for greenhub.

---
+++ greenhub/__init__.py
@@ -0,0 +1,3 @@
+NAME = "greenhub"
+
+VERSION = "0.0.1"
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="greenhub",
+    version="0.0.1",
+    author="Alvaro Mateo",
+    author_email="alvaromateo9@gmail.com",
+    description="Tool that manages your local commits to push a few each day instead of all at the same time.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gbenejam/greenhub",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3"
+    )
+)
